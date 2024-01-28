ad4ce44483ec1f8674ef05f97e3d32d499292083
Lucas <lucas_maitan@hotmail.com>
2024-01-28 09:51:54 -0300

    Alteração de layout da pagina

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -13,3 +14,58 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+/* codigo para rolagem automatica */
+
+document.addEventListener("DOMContentLoaded", function() {
+    var botaoRolar = document.getElementById('bt-contato');
+
+    botaoRolar.addEventListener('click', function() {
+        var conteudo = document.querySelector('.email');
+        var inicio = window.pageYOffset;
+        var destino = conteudo.offsetTop;
+        var duracao = 1600; // duração em milissegundos
+
+        function animarRolagem(tempoAtual) {
+            var tempoPassado = tempoAtual - inicioTempo;
+            var progresso = tempoPassado / duracao;
+            
+            if (progresso < 1) {
+                window.scrollTo(0, inicio + (destino - inicio) * progresso);
+                requestAnimationFrame(animarRolagem);
+            } else {
+                window.scrollTo(0, destino);
+            }
+        }
+        var inicioTempo = performance.now();
+        requestAnimationFrame(animarRolagem);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var botaoRolar = document.getElementById('bt-sobre');
+
+    botaoRolar.addEventListener('click', function() {
+        var conteudo = document.querySelector('.sobre');
+        var inicio = window.pageYOffset;
+        var destino = conteudo.offsetTop;
+        var duracao = 1000; // duração em milissegundos
+
+        function animarRolagem(tempoAtual) {
+            var tempoPassado = tempoAtual - inicioTempo;
+            var progresso = tempoPassado / duracao;
+
+            if (progresso < 1) {
+                window.scrollTo(0, inicio + (destino - inicio) * progresso);
+                requestAnimationFrame(animarRolagem);
+            } else {
+                window.scrollTo(0, destino);
+            }
+        }
+        var inicioTempo = performance.now();
+        requestAnimationFrame(animarRolagem);
+    });
+});
+
+
+
